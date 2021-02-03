a5ab13376549aa525d476c2b18ef7abf406acca6
Lorant Lajos <lorant.lajos@evosoft.com>
2021-02-03 16:26:49 +0100

    Exrcise 135 is added.

---
+++ Basic_Part_1/Exercise_135.py
@@ -0,0 +1,14 @@
+# Write a Python program to print a variable without spaces between values. Go to the editor
+# Sample value : x =30
+# Expected output : Value of x is "30"
+
+x = 30
+print('Value of x is "{}"'.format(x))
+
+# The format() method formats the specified value(s) and insert them inside the string's placeholder.
+
+# The placeholder is defined using curly brackets: {}.
+
+# The placeholders can be identified using named indexes {price}, numbered indexes {0}, or even empty placeholders {}.
+
+# The format() method returns the formatted string.
