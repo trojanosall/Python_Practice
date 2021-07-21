ff1ca2350bc6f6307b9a5db48bc8d0b9f2c1ffa7
Lorant Lajos <lorant.lajos@evosoft.com>
2021-07-21 16:18:07 +0200

    Exercise 140 is done.

---
+++ Basic_Part_1/Solved/Exercise_140.py
@@ -0,0 +1,9 @@
+# Write a Python program to convert an integer to binary keep leading zeros.
+# Sample data : x=12
+# Expected output : 00001100
+# 0000001100
+
+x = 12
+print(format(x, '08b'))
+print(format(x, '010b'))
+
