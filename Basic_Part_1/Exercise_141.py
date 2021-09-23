73b1a27d526275d33837adf7045dc4ceed239ea3
Lorant Lajos <lorant.lajos@evosoft.com>
2021-09-23 12:07:20 +0200

    Exercise 141 is solved

--- Basic_Part_1/Exercise_141.py
+++ Basic_Part_1/Exercise_141.py
@@ -1,2 +1,17 @@
-import sys
->>> print(sys.path)
+# Write a python program to convert decimal to hexadecimal.
+# Sample decimal number: 30, 4
+# Expected output: 1e, 04
+
+number1 = 30
+number2 = 4
+
+def ConverterDecimalToHexa(decimalNumber):
+    return hex(decimalNumber)
+
+print(ConverterDecimalToHexa(number1))
+print(ConverterDecimalToHexa(number2))
+
+x = 30
+print(format(x, '02x'))
+x = 4
+print(format(x, '02x'))
